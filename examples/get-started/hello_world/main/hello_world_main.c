4ee9be93879d5c93fb55501891f0064997501c68
Zim Kalinowski <zim.kalinowski@espressif.com>
2023-03-13 09:44:03 +0100

    just try

--- examples/get-started/hello_world/main/hello_world_main.c
+++ examples/get-started/hello_world/main/hello_world_main.c
@@ -35,6 +35,10 @@ void app_main(void)
         return;
     }
 
+    printf("SIZE OF size_t           : %d", sizeof(size_t));
+    printf("SIZE OF long unsigned int: %d", sizeof(long unsigned int));
+    printf("SIZE OF unsigned int     : %d", sizeof(unsigned int));
+
     printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
