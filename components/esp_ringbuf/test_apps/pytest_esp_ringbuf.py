84e371d46af8f9f75058aab1e22f800012901f00
Zim Kalinowski <zim.kalinowski@espressif.com>
2023-03-16 13:54:42 +0100

    qemu: enable ringbuf

--- components/esp_ringbuf/test_apps/pytest_esp_ringbuf.py
+++ components/esp_ringbuf/test_apps/pytest_esp_ringbuf.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 
+from time import sleep
+
 import pytest
 from pytest_embedded import Dut
 
@@ -18,3 +20,12 @@ from pytest_embedded import Dut
 )
 def test_esp_ringbuf(dut: Dut) -> None:
     dut.run_all_single_board_cases()
+
+
+@pytest.mark.esp32
+@pytest.mark.qemu
+def test_qemu(dut: Dut) -> None:
+    dut.expect_exact('Press ENTER to see the list of tests')
+    sleep(1)
+    dut.write('![qemu-ignore]')
+    dut.expect_unity_test_output(timeout=300)
