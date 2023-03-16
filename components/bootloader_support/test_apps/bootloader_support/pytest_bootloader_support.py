a287dfabbb8c537d1e8de5a59f8bc60b159cef50
Zim Kalinowski <zim.kalinowski@espressif.com>
2023-03-16 14:13:00 +0100

    qemu: enable bootloader_support test

--- components/bootloader_support/test_apps/bootloader_support/pytest_bootloader_support.py
+++ components/bootloader_support/test_apps/bootloader_support/pytest_bootloader_support.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 
+from time import sleep
+
 import pytest
 from pytest_embedded import Dut
 
@@ -9,3 +11,13 @@ from pytest_embedded import Dut
 @pytest.mark.supported_targets
 def test_bootloader_support(dut: Dut) -> None:
     dut.run_all_single_board_cases()
+
+
+@pytest.mark.qemu
+@pytest.mark.esp32
+def test_bootloader_support_qemu(dut: Dut) -> None:
+    dut.expect_exact('Press ENTER to see the list of tests')
+    # dut may not be ready to accept input, so adding the delay until handled in pytest embedded
+    sleep(1)
+    dut.write('![qemu-ignore]')
+    dut.expect_unity_test_output(timeout=300)
