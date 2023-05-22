1dbbe7332331ea084a430b9d26aa01a34b0d7049
Zim Kalinowski <zim.kalinowski@espressif.com>
2023-05-22 12:12:41 +0200

    enabling esp_event on quemu

--- components/esp_event/test_apps/pytest_esp_event.py
+++ components/esp_event/test_apps/pytest_esp_event.py
@@ -11,3 +11,12 @@ from pytest_embedded import Dut
 @pytest.mark.generic
 def test_esp_event(dut: Dut) -> None:
     dut.run_all_single_board_cases()
+
+@pytest.mark.esp32
+@pytest.mark.qemu
+def test_qemu(dut: Dut) -> None:
+    dut.expect_exact('Press ENTER to see the list of tests')
+    # dut may not be ready to accept input, so adding the delay until handled in pytest embedded (RDT-328)
+    # sleep(1)
+    dut.write('![qemu-ignore]')
+    dut.expect_unity_test_output(timeout=300)
